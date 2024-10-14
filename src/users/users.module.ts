3495a82ffaa1976aba37cb24da2fc0328d90a828
NadiyPro <siroviyn13@gmail.com>
2024-10-14 19:49:37 +0300

    create

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersAdminController } from './users-admin.controller';
+import { UsersAdminService } from './users-admin.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, UsersAdminController],
+  providers: [UsersService, UsersAdminService],
 })
 export class UsersModule {}

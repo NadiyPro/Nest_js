850b877c9422dfc73e7af5abff86259b9a58afe1
NadiyPro <siroviyn13@gmail.com>
2024-10-14 18:58:17 +0300

    create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  // У цьому полі визначаються інші модулі, які імпортуються в цей модуль.
+  // Оскільки тут імпортів немає, масив порожній
+  // controllers: [AppController],
+  // // Тут вказані контролери, які використовуються в цьому модулі.
+  // // У даному випадку це AppController.
+  // providers: [AppService],
+  // // Це поле визначає провайдери (сервіси),
+  // // які будуть доступні в модулі. У даному випадку це AppService
 })
 export class AppModule {}
+// клас AppModule, який позначений як модуль завдяки декоратору @Module.
+// Він поєднує контролери і провайдери, які входять до складу програми
+//Як працює цей модуль:
+// AppController: це контролер, який обробляє HTTP-запити. Контролери містять маршрути (шляхи),
+// які відповідають на запити від клієнтів.
+// AppService: це провайдер, який містить бізнес-логіку програми.
+// Контролер може використовувати цей сервіс для отримання або обробки даних.
+// У результаті, AppModule — це основний модуль програми, який об'єднує контролер та сервіс,
+// щоб забезпечити основний функціонал вашої NestJS програми.

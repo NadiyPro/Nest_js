10150a4cd551e32f20e971b10d8b6e8324c17280
NadiyPro <siroviyn13@gmail.com>
2024-10-15 22:08:53 +0300

    create

--- src/users/dto/req/create-user.req.dto.ts
+++ src/users/dto/req/create-user.req.dto.ts
@@ -1,7 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserReqDto {
+  @ApiProperty({ example: 'John Doe', description: 'The name of the User' })
   readonly name: string;
-  readonly age?: number;
+  @ApiProperty({
+    example: 30,
+    description: 'The age of the User',
+    minimum: 0,
+    maximum: 120,
+  })
+  readonly age: number;
+  @ApiProperty({
+    example: 'test@test.com',
+    description: 'The email of the User',
+  })
   readonly email: string;
+  @ApiProperty({ example: 'password', description: 'The password of the User' })
   readonly password: string;
+  @ApiProperty({
+    example: 'admin',
+    description: 'The role of the User',
+    enum: ['admin', 'user'],
+  })
   readonly role: string;
 }

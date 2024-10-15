5e790c88e98003109599c10d754d046138133d52
NadiyPro <siroviyn13@gmail.com>
2024-10-15 16:44:55 +0300

    create

--- src/main.ts
+++ src/main.ts
@@ -8,25 +8,34 @@ async function bootstrap() {
   //Ця команда створює новий екземпляр додатка на основі AppModule за допомогою NestFactory.
   // Вона ініціалізує всі компоненти програми, налаштовує залежності та маршрутизацію.
 
-  const config = new DocumentBuilder()
-    .setTitle('March-2024 NestJS')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
+  const config = new DocumentBuilder() //  Створює новий об'єкт для побудови конфігурації Swagger
+    .setTitle('March-2024 NestJS') // Встановлює заголовок API документації
+    .setDescription('The cats API description') // опис нашого API
+    .setVersion('1.0') // версія нашого API
     .addBearerAuth({
-      in: 'header',
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-    })
-    .build();
+      in: 'header', //вказує, що токен буде передаватися в заголовку HTTP-запиту.
+      type: 'http', // тип авторизації — HTTP
+      scheme: 'bearer', // Схема авторизації — Bearer
+      bearerFormat: 'JWT', //  Формат токена — JWT (JSON Web Token).
+    }) // Додає налаштування для авторизації типу Bearer, що використовується для JWT токенів
+    .build(); // Створює остаточну конфігурацію на основі вказаних параметрів
   const document = SwaggerModule.createDocument(app, config);
+  // Генерує документ Swagger (OpenAPI специфікацію)
+  // на основі нашого NestJS додатку (app) та створеної конфігурації (config).
   SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
       docExpansion: 'list',
+      // Встановлює, як розкривати розділи документації.
+      // 'list' означає, що всі розділи будуть показані у вигляді списку і згорнуті за замовчуванням
       defaultModelsExpandDepth: 7,
+      // Визначає глибину, до якої будуть розкриватися моделі даних.
+      // Число 7 означає, що вкладені моделі будуть розкриті до 7 рівнів.
       persistAuthorization: true,
+      // Дозволяє зберігати стан авторизації між перезавантаженнями сторінки.
+      // Тобто, якщо ви ввели токен авторизації, він залишиться активним навіть після оновлення сторінки.
     },
   });
+  // Налаштовує маршрут /docs, за яким буде доступна ваша Swagger документація
   const port = 3000;
   const host = 'localhost';
 

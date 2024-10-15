749847f22a0923eef494085b60016c884d8a2918
NadiyPro <siroviyn13@gmail.com>
2024-10-15 22:39:20 +0300

    create

--- src/users/dto/req/create-user.req.dto.ts
+++ src/users/dto/req/create-user.req.dto.ts
@@ -1,14 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class CreateUserReqDto {
-  @ApiProperty()
   readonly name: string;
-  @ApiProperty()
   readonly age?: number;
-  @ApiProperty()
   readonly email: string;
-  @ApiProperty()
   readonly password: string;
-  @ApiProperty()
   readonly role: string;
 }

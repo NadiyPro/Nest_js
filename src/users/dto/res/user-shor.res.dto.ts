dd34cbca4f13195798cd1d738a36b065bb265663
NadiyPro <siroviyn13@gmail.com>
2024-10-16 18:11:09 +0300

    create

---
+++ src/users/dto/res/user-shor.res.dto.ts
@@ -0,0 +1,7 @@
+import { PickType } from '@nestjs/swagger';
+
+import { UserBaseResDto } from './user-base.res.dto';
+
+export class UserShorResDto extends PickType(UserBaseResDto, ['id', 'name']) {}
+// PickType() дозволяє "вибрати" тільки конкретні поля з базового класу
+// і використовувати їх у новому DTO.

511b0bcd1d58a38330067da106db950ec734f3f9
NadiyPro <siroviyn13@gmail.com>
2024-10-18 20:03:39 +0300

    create

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { ArticlesModule } from './articles/articles.module';
 import { CommentsModule } from './comments/comments.module';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configs/configuration';

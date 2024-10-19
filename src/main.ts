e6bbf35ab212d9ad76f1dfb40a47f5e1bd6c72e3
NadiyPro <siroviyn13@gmail.com>
2024-10-19 14:13:15 +0300

    create

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { SwaggerHelper } from './common/helpers/swagger.helper';
 import { AppConfig } from './configs/config.type';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
